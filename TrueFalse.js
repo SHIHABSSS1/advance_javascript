4b6e4d1c9c9407f1becc7d0d3e5c528f9797e728
SHIHABSSS1 <shihabhossain596@gmail.com>
2025-07-10 11:46:52 +0600

    pushing

---
+++ TrueFalse.js
@@ -0,0 +1,19 @@
+const age = 4;
+if(age) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+//value thakle true dhore ney , na thakle false
+const names = "";
+if(names) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+
+
+// falsy values: 0, "", undefined, null, NaN
+// truthy values: "0", " ", [], {}, true, "false"
