0316b1b15aff5248c1a2562bd702c53e7867172f
SHIHABSSS1 <shihabhossain596@gmail.com>
2025-07-10 11:58:05 +0600

    nullorundefined

---
+++ nullOrUndefined.js
@@ -0,0 +1,18 @@
+                                                    //example one
+let names;
+console.log(names);
+
+                                                    // example two
+function add(num1, num2){
+    console.log(num1 + num2);
+    // return num1 + num2;
+}
+const result = add(5, 10);
+console.log(result);
+
+                                                    //exampe three
+ names = null;
+if(names){
+    console.log("condition is true");
+}                                                   
+
